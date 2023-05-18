25c4c71422e8d20a46e0adbeffaa52f7716afa2a
Katrina JMD <personal.k.konz@gmail.com>
2023-05-18 18:45:06 +0200

    PROJECTNo6 preparedData for Recommendation

--- Project No.6/config.py
+++ Project No.6/config.py
@@ -1,2 +1,5 @@
-clientId = "e52e5070a6f34a7fbcfcf1bf3dd36139"
-clientSecret = "ab0705a1fbae434594e723591652ccda"
+# clientId = "e52e5070a6f34a7fbcfcf1bf3dd36139"
+# clientSecret = "ab0705a1fbae434594e723591652ccda"
+
+clientId = "d33017fdcf2843bfadab2701856ec879"
+clientSecret = "fdd348060c534742be5f4a448d70474b"
